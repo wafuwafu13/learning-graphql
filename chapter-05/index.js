9cf16c5c13a55d43cb596587bd909f07461c31b9
wafuwafu13 <mariobaske@i.softbank.jp>
2021-07-02 11:24:10 +0900

    5.2.4.2 多対多のコネクション

--- chapter-05/index.js
+++ chapter-05/index.js
@@ -6,6 +6,7 @@ const typeDefs = `
 	  name: String
 	  avater: String
 	  postedPhotos: [Photo!]!
+	  inPhotos: [Photo!]!
   }
 
   enum PhotoCategory {
@@ -23,6 +24,7 @@ const typeDefs = `
 	  description: String
 	  category: PhotoCategory!
 	  postedBy: User!
+	  taggedUsers: [User!]!
   }
 
   input PostPhotoInput {
@@ -71,6 +73,13 @@ let photos = [
 	}
 ]
 
+let tags = [
+	{ "photoID": "1", "userID": "gPlake" },
+	{ "photoID": "2", "userID": "sSchmidt" },
+	{ "photoID": "2", "userID": "mHattrup" },
+	{ "photoID": "2", "userID": "gPlake" },
+]
+
 const resolvers = {
 	Query: {
 		totalPhotos: () => photos.length,
@@ -93,13 +102,21 @@ const resolvers = {
 		url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
 		postedBy: parent => {
 			return users.find(u => u.githubLogin === parent.githubUser)
-		}
+		},
+		taggedUsers: parent => tags
+	        .filter(tag => tag.photoID === parent.id)
+			.map(tag => tag.userID)
+			.map(userID => users.find(u => u.githubLogin === userID))
 	},
 
 	User: {
 		postedPhotos: parent => {
 			return photos.filter(p => p.githubUser === parent.githubLogin)
-		}
+		},
+		inPhotos: parent => tags
+		    .filter(tag => tag.userID === parent.id)
+			.map(tag => tag.photoID)
+			.map(photoID => photos.find(p => p.id === photoID))
 	}
 }
 
