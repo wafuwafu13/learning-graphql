f52c2b75ab6368229f2339fa9e05ff39c38dab8d
wafuwafu13 <mariobaske@i.softbank.jp>
2021-07-03 11:05:13 +0900

    5.5.4.2 postPhotoミューテーション

--- chapter-05/resolvers/Type.js
+++ chapter-05/resolvers/Type.js
@@ -41,14 +41,11 @@ let tags = [
 
 module.exports = {
 	Photo: {
-		url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
-		postedBy: parent => {
-			return users.find(u => u.githubLogin === parent.githubUser)
-		},
-		taggedUsers: parent => tags
-	        .filter(tag => tag.photoID === parent.id)
-			.map(tag => tag.userID)
-			.map(userID => users.find(u => u.githubLogin === userID))
+        id: parent => parent.id || parent._id,
+        url: parent => `/img/photos/${parent._id}.jpg`,
+		postedBy: (parent, args, { db }) =>
+            db.collection('users')
+                .findOne({ githubLogin: parent.userID }),
 	},
 
 	User: {
