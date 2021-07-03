f9ebff8045e17bbed45b41765eeeac4b4262638e
wafuwafu13 <mariobaske@i.softbank.jp>
2021-07-03 10:19:28 +0900

    5.5.3 githubAuthミューテーション

--- chapter-05/resolvers/Mutation.js
+++ chapter-05/resolvers/Mutation.js
@@ -1,3 +1,5 @@
+const { authorizeWithGithub } = require('../lib')
+
 let photos = [
 	{
 		"id": "1",
@@ -35,5 +37,37 @@ module.exports = {
 		photos.push(newPhoto)
 
 		return newPhoto
-	}
+	},
+
+	async githubAuth(parent, { code }, { db }) {
+
+		let {
+		  message,
+		  access_token,
+		  avatar_url,
+		  login,
+		  name
+		} = await authorizeWithGithub({
+		  client_id: process.env.CLIENT_ID,
+		  client_secret: process.env.CLIENT_SECRET,
+		  code
+		})
+	
+		if (message) {
+		  throw new Error(message)
+		}
+	
+		let latestUserInfo = {
+		  name,
+		  githubLogin: login,
+		  githubToken: access_token,
+		  avatar: avatar_url
+		}
+	
+		const { ops:[user] } = await db
+		  .collection('users')
+		  .replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true })
+	
+		return { user, token: access_token }
+	},
 }
