09b2bbe60bcd8afdd45e7e2eeec1b1da54e4b294
wafuwafu13 <mariobaske@i.softbank.jp>
2021-07-03 12:02:45 +0900

    6.3.2 Apollo Clientの設定

--- chapter-06/photo-share-client/src/index.js
+++ chapter-06/photo-share-client/src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ApolloClient, { gql } from 'apollo-boost';
+
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' });
+const query = gql`
+{
+	totalUsers
+	totalPhotos
+}
+`
+
+client.query({query})
+  .then(({ data }) => console.log('data', data))
+  .catch(console.error)
+
+console.log('cache', client.extract())
+client.query({ query })
+  .then(() => console.log('cache', client.extract()))
+  .catch(console.error)
 
 ReactDOM.render(
   <React.StrictMode>
