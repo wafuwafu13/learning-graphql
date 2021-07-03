79aa1757ec945ed406f36ad9f59e822c02212dd2
wafuwafu13 <mariobaske@i.softbank.jp>
2021-07-03 12:05:29 +0900

    6.3.2 Apollo Clientの設定

--- chapter-06/photo-share-client/src/index.js
+++ chapter-06/photo-share-client/src/index.js
@@ -1,33 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' });
-const query = gql`
-{
-	totalUsers
-	totalPhotos
-}
-`
 
-client.query({query})
-  .then(({ data }) => console.log('data', data))
-  .catch(console.error)
-
-console.log('cache', client.extract())
-client.query({ query })
-  .then(() => console.log('cache', client.extract()))
-  .catch(console.error)
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
