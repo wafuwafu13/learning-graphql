14d3bbe38e65574ef931106724b133f8728aed10
wafuwafu13 <mariobaske@i.softbank.jp>
2021-07-03 09:42:22 +0900

    5.4.2 コンテキストへのデータベースの追加

--- chapter-05/index.js
+++ chapter-05/index.js
@@ -2,63 +2,34 @@ const { ApolloServer } = require(`apollo-server-express`)
 const express = require(`express`)
 const expressPlayground = require(`graphql-playground-middleware-express`).default
 const { readFileSync } = require(`fs`)
+const { MongoClient } = require(`mongodb`)
+require(`dotenv`).config()
 
 const typeDefs = readFileSync(`./typeDefs.graphql`, `UTF-8`)
-
-let users = [
-	{ "githubLogin": "mHattrup", "name": "Mike Hattrup"},
-	{ "githubLogin": "gPlake", "name": "Glen Plake"},
-	{ "githubLogin": "sSchmidt", "name": "Scot Schmidt"}
-]
-
-let _id = 0
-let photos = [
-	{
-		"id": "1",
-		"name": "Dropping the Heart Chute",
-		"description": "The heart chute is one of my favorite chutes",
-		"category": "ACTION",
-		"githubUser": "gPlake",
-		"created": "3-28-1977"
-	},
-	{
-		"id": "2",
-		"name": "Enjoying the sunshine",
-		"category": "SELFIE",
-		"githubUser": "sSchmidt",
-		"created": "1-2-1985"
-	},
-	{
-		"id": "3",
-		"name": "Gunbarrel 25",
-		"description": "25 laps on gunbarrel today",
-		"category": "LANDSCAPE",
-		"githubUser": "sSchmidt",
-		"created": "2018-04-15T19:09:57.308Z"
-	}
-]
-
-let tags = [
-	{ "photoID": "1", "userID": "gPlake" },
-	{ "photoID": "2", "userID": "sSchmidt" },
-	{ "photoID": "2", "userID": "mHattrup" },
-	{ "photoID": "2", "userID": "gPlake" },
-]
-
 const resolvers = require(`./resolvers`)
 
-let app = express()
+async function start() {
+    const app = express()
+	const MONGO_DB = process.env.DB_HOST
 
-const server = new ApolloServer({
-	typeDefs,
-	resolvers
-})
+	const client = await MongoClient.connect(
+		MONGO_DB,
+		{ useNewUrlParser: true }
+	)
+	const db = client.db()
 
-server.applyMiddleware({ app })
+	const context = { db }
 
-app.get(`/`, (req, res) => res.end(`Welcome to the PhotoShareAPI`))
-app.get(`/playground`, expressPlayground({ endpoint: `/graphql` }))
+	const server = new ApolloServer({ typeDefs, resolvers, context })
 
-app.listen({ port: 4000 }, () =>
-    console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
-)
+	server.applyMiddleware({ app })
+
+    app.get(`/`, (req, res) => res.end(`Welcome to the PhotoShareAPI`))
+    app.get(`/playground`, expressPlayground({ endpoint: `/graphql` }))
+
+    app.listen({ port: 4000 }, () =>
+        console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
+    )
+}
+
+start()
--- chapter-05/resolvers/Query.js
+++ chapter-05/resolvers/Query.js
@@ -1,30 +1,19 @@
-let photos = [
-	{
-		"id": "1",
-		"name": "Dropping the Heart Chute",
-		"description": "The heart chute is one of my favorite chutes",
-		"category": "ACTION",
-		"githubUser": "gPlake",
-		"created": "3-28-1977"
-	},
-	{
-		"id": "2",
-		"name": "Enjoying the sunshine",
-		"category": "SELFIE",
-		"githubUser": "sSchmidt",
-		"created": "1-2-1985"
-	},
-	{
-		"id": "3",
-		"name": "Gunbarrel 25",
-		"description": "25 laps on gunbarrel today",
-		"category": "LANDSCAPE",
-		"githubUser": "sSchmidt",
-		"created": "2018-04-15T19:09:57.308Z"
-	}
-]
-
 module.exports = {
-	totalPhotos: () => photos.length,
-	allPhotos: () => photos
+	totalPhotos: (parent, args, { db }) => 
+      db.collection('photos')
+        .estimatedDocumentCount(),
+
+    allPhotos: (parent, args, { db }) =>
+      db.collection('photos')
+        .find()
+        .toArray(),
+	
+	totalUsers: (parent, args, { db }) =>
+	  db.collection('users')
+		.estimatedDocumentCount(),
+  
+	allUsers: (parent, args, { db }) =>
+	  db.collection('users')
+	    .find()
+	    .toArray(),
 }
