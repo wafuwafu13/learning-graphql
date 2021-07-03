8dd08643ee7d0a89f8eeae03542b03fe1a1ee02c
wafuwafu13 <mariobaske@i.softbank.jp>
2021-07-03 12:53:36 +0900

    Queryコンポーネント

---
+++ chapter-06/photo-share-client/src/Users.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Query } from "react-apollo";
+import { ROOT_QUERY } from "./App";
+
+const Users = () => {
+  return (
+    <Query query={ROOT_QUERY}>
+      {({ data, loading, refetch }) =>
+        loading ? (
+          <p>loading users...</p>
+        ) : (
+          <UserList
+            count={data.totalUsers}
+            users={data.allUsers}
+            refetchUsers={refetch}
+          />
+        )
+      }
+    </Query>
+  );
+};
+
+const UserList = ({ count, users, refetchUsers }) => (
+  <div>
+    <p>{count} Users</p>
+	<button onClick={() => refetchUsers()}>Refetch</button>
+    <ul>
+      {users.map((user) => (
+        <UserListItem
+          key={user.githubLogin}
+          name={user.name}
+          avatar={user.avatar}
+        />
+      ))}
+    </ul>
+  </div>
+);
+
+const UserListItem = ({ name, avatar }) => (
+  <li>
+    <img src={avatar} width={48} height={48} alt="" />
+    {name}
+  </li>
+);
+
+export default Users;
