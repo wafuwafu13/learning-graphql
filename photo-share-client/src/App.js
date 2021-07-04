79168d6ab77403aa26a8bde6242dae72df5120d2
wafuwafu13 <mariobaske@i.softbank.jp>
2021-07-05 08:41:11 +0900

    7.1.2.2 ユーザーの追加を待ち受ける

--- photo-share-client/src/App.js
+++ photo-share-client/src/App.js
@@ -1,29 +1,63 @@
-import React from 'react'
-import Users from './Users'
-import { BrowserRouter } from 'react-router-dom'
-import AuthorizedUser from './AuthorizedUser'
-import { gql } from 'apollo-boost'
+import React, { Component } from "react";
+import Users from "./Users";
+import { BrowserRouter } from "react-router-dom";
+import AuthorizedUser from "./AuthorizedUser";
+import { gql } from "apollo-boost";
+import { withApollo } from "react-apollo";
 
 export const ROOT_QUERY = gql`
-    query allUsers {
-        totalUsers        
-        allUsers { ...userInfo }
-        me { ...userInfo }
+  query allUsers {
+    totalUsers
+    allUsers {
+      ...userInfo
     }
-
-    fragment userInfo on User {
-        githubLogin
-        name
-        avatar
+    me {
+      ...userInfo
     }
-`
+  }
 
-const App = () => 
-  <BrowserRouter>
-    <div>
+  fragment userInfo on User {
+    githubLogin
+    name
+    avatar
+  }
+`;
+
+const LISTEN_FOR_USERS = gql`
+  subscription {
+    newUser {
+      githubLogin
+      name
+      avatar
+    }
+  }
+`;
+
+class App extends Component {
+  componentDidMount() {
+    let { client } = this.props;
+    this.listenForUsers = client
+      .subscribe({ query: LISTEN_FOR_USERS })
+      .subscribe(({ data: { newUser } }) => {
+        const data = client.readQuery({ query: ROOT_QUERY });
+        data.totalUsers += 1;
+        data.allUsers = [...data.allUsers, newUser];
+        client.writeQuery({ query: ROOT_QUERY, data });
+      });
+  }
+
+  componentWillUnmount() {
+    this.listenForUsers.unsubscribe();
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
         <AuthorizedUser />
         <Users />
-    </div>
-  </BrowserRouter>  
+      </BrowserRouter>
+    );
+  }
+}
 
-export default App;
+export default withApollo(App);
--- photo-share-client/src/Users.js
+++ photo-share-client/src/Users.js
@@ -31,21 +31,11 @@ const Users = () => {
   );
 };
 
-const updateUserCache = (cache, { data:{ addFakeUsers } }) => {
-    let data = cache.readQuery({ query: ROOT_QUERY })
-    data.totalUsers += addFakeUsers.length
-    data.allUsers = [
-        ...data.allUsers,
-        ...addFakeUsers
-    ]
-    cache.writeQuery({ query: ROOT_QUERY, data })
-}
-
 const UserList = ({ count, users, refetchUsers }) => (
   <div>
     <p>{count} Users</p>
     <button onClick={() => refetchUsers()}>Refetch</button>
-    <Mutation mutation={ADD_FAKE_USERS_MUTATION} variables={{ count: 1 }} update={updateUserCache}>
+    <Mutation mutation={ADD_FAKE_USERS_MUTATION} variables={{ count: 1 }}>
       {(addFakeUsers) => <button onClick={addFakeUsers}>Add Fake User</button>}
     </Mutation>
     <ul>
