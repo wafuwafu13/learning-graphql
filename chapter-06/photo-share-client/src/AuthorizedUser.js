0ca44c9070b52897deb049dab491e5e907db108e
wafuwafu13 <mariobaske@i.softbank.jp>
2021-07-04 11:52:32 +0900

    6.5.3 キャッシュの更新

--- chapter-06/photo-share-client/src/AuthorizedUser.js
+++ chapter-06/photo-share-client/src/AuthorizedUser.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
-import { Query, Mutation } from "react-apollo";
+import { Query, Mutation, withApollo } from "react-apollo";
 import { ROOT_QUERY } from "./App";
 import { gql } from "apollo-boost";
+import { compose } from "recompose"
 
 const GITHUB_AUTH_MUTATION = gql`
   mutation githubAuth($code: String!) {
@@ -21,7 +22,7 @@ const CurrentUser = ({ name, avatar, logout }) => (
 );
 
 const Me = ({ logout, requestCode, signingIn }) => (
-  <Query query={ROOT_QUERY}>
+  <Query query={ROOT_QUERY} fetchPolicy="cache-and-network">
     {({ loading, data }) => loading ?
         <p>loading... </p> : 
 		data.me ? 
@@ -68,7 +69,12 @@ class AuthorizedUser extends Component {
             <Me
               signingIn={this.state.signingIn}
               requestCode={this.requestCode}
-              logout={() => localStorage.removeItem("token")}
+              logout={() => {
+				localStorage.removeItem('token')
+				let data = this.props.client.readQuery({ query: ROOT_QUERY })
+				data.me = null
+				this.props.client.writeQuery({ query: ROOT_QUERY, data })
+			  }}
             />
           );
         }}
@@ -77,4 +83,4 @@ class AuthorizedUser extends Component {
   }
 }
 
-export default withRouter(AuthorizedUser);
+export default compose(withApollo, withRouter)(AuthorizedUser)
